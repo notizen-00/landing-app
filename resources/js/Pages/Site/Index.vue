8e8cd10d623b26e1872ceafd96e8cf87526edaa1
dipta ikromi m <dipta.ikromi@unej.ac.id>
2023-08-22 11:38:49 +0700

    carousel layout absolute

--- resources/js/Pages/Site/Index.vue
+++ resources/js/Pages/Site/Index.vue
@@ -5,8 +5,7 @@
 
 <template>
     <SiteLayout>
-        
-        <v-carousel show-arrows="hover"  hide-delimiters class="relative overflow-visible">
+        <v-carousel show-arrows="hover" hide-delimiters class="relative">
             <v-carousel-item
               src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
                 cover
@@ -22,16 +21,55 @@
             cover
             ></v-carousel-item>
 
-          
+<!-- 
+             
+
+              <v-text-field
+              label="Kecamatan"
+              prepend-inner-icon="mdi-magnify"
+              variant="outlined"
+              class="absolute top-1 right-0 w-1/2 bg-transparent  z-50"
+            ></v-text-field> -->
+
+            <div class="absolute flex w-1/2 h-12 left-1/4 justify-center mt-2 rounded-full bg-slate-100">
+              <div class="w-1/4 h-3 bg-slate-500">  
+                <v-text-field solo
+                  variant="filled"
+                  class="h-3"
+                >
+                    
+              </v-text-field>
+            </div>
+              <div class="w-1/4">
                 <v-text-field
-                label="Ketikan Sesuatu ..."
+                label="Subsektor"
                 prepend-inner-icon="mdi-magnify"
                 variant="outlined"
-                class="absolute -bottom-10 left-1/4 right-1/4  z-50 bg-white rounded-md"
+                class="bg-transparent mt-3  z-50"
               ></v-text-field>
+              </div>
+              
+              <div class="w-1/4">
+                  <v-text-field
+                  label="Kecamatan"
+                  prepend-inner-icon="mdi-magnify"
+                  variant="outlined"
+                  class="bg-transparent mt-3 z-50"
+                ></v-text-field>
+              </div>
+            </div>
+             
              
           </v-carousel>
 
     </SiteLayout>
 
 </template>
+<style scoped>
+.v-text-field .v-input__control .v-input__slot {
+  min-height: auto !important;
+  display: flex !important;
+  align-items: center !important;
+}
+
+</style>
